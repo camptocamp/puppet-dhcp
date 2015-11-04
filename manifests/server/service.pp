4dbb5a09e23e56fc8ed7ff04fc1cfcdc17932786
Mickaël Canévet <mickael.canevet@camptocamp.com>
2015-11-04 11:11:53 +0100

    Fix convergence on Debian 8

--- manifests/server/service.pp
+++ manifests/server/service.pp
@@ -10,10 +10,17 @@ class dhcp::server::service {
   validate_string($dhcp::params::service_pattern)
   validate_re($dhcp::params::service_pattern, '^\S+$')
 
+  if $::operatingsystem == 'Debian' and $::operatingsystemmajrelease == '8' {
+    $provider = 'debian'
+  } else {
+    $provider = undef
+  }
+
   service {'dhcpd':
-    ensure  => running,
-    name    => $dhcp::params::srv_dhcpd,
-    enable  => true,
-    pattern => $dhcp::params::service_pattern,
+    ensure   => running,
+    provider => $provider,
+    name     => $dhcp::params::srv_dhcpd,
+    enable   => true,
+    pattern  => $dhcp::params::service_pattern,
   }
 }
